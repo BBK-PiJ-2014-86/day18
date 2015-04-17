d11ad47e4497f1ca5b9e281477d277043752b2a6
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-17 21:03:08 +0100

    implementation (mirroring API)

--- ExecutorImpl.java
+++ ExecutorImpl.java
@@ -1,12 +1,37 @@
 package day18;
 
+import java.util.Queue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ExecutorImpl implements Executor {
+	
+	final Queue <Runnable> queue = new LinkedBlockingQueue <Runnable>(); 
+	Runnable current;
 
 	@Override
-	public void execute(Runnable command) {
-		// TODO Auto-generated method stub
+	public synchronized void execute(Runnable command) {
+
+		queue.offer(new Runnable () {
+
+			@Override
+			public void run() {
+				
+				try {
+					command.run();
+				} finally {
+					next();
+				}
+			}
+
+			private void next() {
+				if ((current= queue.poll()) != null) {
+					current
+				}
+				
+			}
+			
+		});
 		
 	}
 	
