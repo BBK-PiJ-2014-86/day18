651b87238c50998b51e85e8f08178f430fd912ea
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-17 20:49:56 +0100

    first version

---
+++ ExecutorImpl.java
@@ -0,0 +1,15 @@
+package day18;
+
+import java.util.concurrent.Executor;
+
+public class ExecutorImpl implements Executor {
+
+	@Override
+	public void execute(Runnable command) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
